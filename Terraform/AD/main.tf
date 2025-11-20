6faf41c7cba5b3316fa6e0664cfc0a244cd4334f
Manuel Ortega <manort70@gmail.com>
2025-11-19 21:48:28 -0500

    Update main.tf

--- Terraform/AD/main.tf
+++ Terraform/AD/main.tf
@@ -1,20 +1,13 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2822
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\margl1440\margr1440\vieww11520\viewh8400\viewkind0
-\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
+provider "ad" {
+  winrm_hostname = "AD01.homelab.local"
+  winrm_username = var.ad_user
+  winrm_password = var.ad_password
+  winrm_port     = 5986
+  winrm_proto    = "https"
+  krb_realm      = "HOMELAB.LOCAL"
+}
 
-\f0\fs24 \cf0 provider "ad" \{\
-  winrm_hostname = "AD01.homelab.local"\
-  winrm_username = var.ad_user\
-  winrm_password = var.ad_password\
-  winrm_port     = 5986\
-  winrm_proto    = "https"\
-  krb_realm      = "HOMELAB.LOCAL"\
-\}\
-\
-resource "ad_computer" "splunk" \{\
-  name                  = "SplunkServer"\
-  organizational_unit   = "OU=Servers,DC=homelab,DC=local"\
-\}}
+resource "ad_computer" "splunk" {
+  name                  = "SplunkServer"
+  organizational_unit   = "OU=Servers,DC=homelab,DC=local"
+}
